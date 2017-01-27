031ca58be57807740ea3aad4d998b81d95593626
Eric Nedervold <nedervoldsoftware@gmail.com>
2017-01-26 16:46:44 -0800

    First code commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+# std-token
+
+TODO: Write description here
+
+## Installation
+
+```
+cabal install --enable-tests
+```
+
+
+## Usage
+
+TODO: Write usage instructions here
+
+## How to run tests
+
+```
+cabal configure --enable-tests && cabal build && cabal test
+```
+
+## Contributing
+
+Pull requests welcome.
---
+++ src/Text/StdToken.hs
@@ -0,0 +1,33 @@
+-- | A datatype for tokens.
+{-# LANGUAGE TemplateHaskell #-}
+module Text.StdToken(
+    StdToken(..),
+    tokenType,
+    tokenText,
+    tokenDeco,
+    mkAlexToken) where
+
+import Control.Lens.TH
+import Data.Bifunctor
+
+-- | A datatype for tokens.  Type synonyms will probably be used to
+-- refer to its specializations.
+data StdToken ty txt deco = Token {
+    _tokenType :: ty,
+    _tokenText :: txt,
+    _tokenDeco :: deco
+    }
+
+makeLenses ''StdToken
+
+instance Functor (StdToken ty txt) where
+    fmap f (Token ty txt deco) = Token ty txt (f deco)
+
+instance Bifunctor (StdToken ty) where
+    bimap f g (Token ty txt deco) = Token ty (f txt) (g deco)
+
+-- | Can be used to create tokens in an Alex scanner that uses the
+-- \"posn\" wrapper.
+mkAlexToken :: ty -> posn -> String -> StdToken ty String posn
+mkAlexToken ty posn txt = Token ty txt posn
+
